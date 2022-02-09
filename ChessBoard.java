081d1846554425beef857c6c7774197261e2372b
Keith Yao <keithyao1012@gmail.com>
2022-02-08 22:15:44 -0500

    Started

---
+++ ChessBoard.java
@@ -0,0 +1,9 @@
+public class ChessBoard {
+    char[][] board;
+    boolean checkMate = false;
+    boolean check = false;
+    boolean nothing =true;
+    public ChessBoard(){
+        board = new char[8][8];
+    }
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+    public static void main (String[] args){
+        ChessBoard board = new ChessBoard();
+
+        //Get User to input every piece
+    }
+}
